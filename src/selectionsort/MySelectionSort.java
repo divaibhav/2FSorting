79562a9ff9c9a86717e7862d90d5dba385202c4a
Vaibhav Diwan <divaibhav@gmail.com>
2021-06-02 10:24:40 +0530

    Initial commit

---
+++ src/selectionsort/MySelectionSort.java
@@ -0,0 +1,38 @@
+package selectionsort;
+
+import java.util.Arrays;
+
+public class MySelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {12,8,16,2,5};
+        MySelectionSort obj = new MySelectionSort();
+        System.out.println("unsorted -->" + Arrays.toString(arr));
+        obj.selectionSort(arr);
+        System.out.println("sorted -->" + Arrays.toString(arr));
+    }
+    public void selectionSort(int[] arr){
+        //step 1
+        int sortedIndex = arr.length;
+        int maximumElement;
+        int maximumElementIndex;
+        //step 2
+        for (int i = 0; i < arr.length; i++) {
+            //step 3 find maximum
+            maximumElement = arr[0];
+            maximumElementIndex = 0;
+            for (int j = 0; j < sortedIndex; j++) {
+                if(maximumElement < arr[j]) {
+                       maximumElement = arr[j];
+                       maximumElementIndex = j;
+                }
+            }
+            System.out.println("maximum = " + maximumElement);
+            //step 4 swap
+            System.out.println("swapping --> " + arr[maximumElementIndex] + " with --> " + arr[sortedIndex - 1]);
+            int temp = arr[maximumElementIndex];
+            sortedIndex--;
+            arr[maximumElementIndex] = arr[sortedIndex];
+            arr[sortedIndex] = temp;
+        }
+    }
+}
